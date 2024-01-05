39bafc6f20c7e4c4f2a1fce63d6640b7a31f2271
Bianca de Castro Mendonça <biancadcastro2501@gmail.com>
2024-01-05 19:51:24 -0300

    removendo os antigos componentes e atualizando o layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
     import "./app.css";
-    import NavBar from "./components/NavBar.svelte"
-    import Footer from "./components/Footer.svelte"
+    import NavBar from "$lib/components/NavBar.svelte"
+    import Footer from "$lib/components/Footer.svelte"
 </script>
 
 <NavBar />
