04470a20b5a54d89cc5a64246214d554e1668a07
Bianca de Castro Mendonça <biancadcastro2501@gmail.com>
2024-02-18 20:47:36 -0300

    adicionando as tags og a pagina de planos

--- src/routes/planos/+page.svelte
+++ src/routes/planos/+page.svelte
@@ -51,6 +51,17 @@
 
 <svelte:head>
     <title>Planos - Jake</title>
+
+    <meta property="og:type" content="website" />
+	<meta property="og:url" content="https://jakebot.com.br/planos" />
+	<meta property="og:title" content="Planos - Jake Bot" />
+	<meta property="og:description" content="Aqui você pode escolher um plano VIP que te dara beneficios como funções extras e mais personalizações do bot!" />
+
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://jakebot.com.br/planos" />
+	<meta property="twitter:title" content="Planos - Jake Bot " />
+	<meta property="twitter:description" content="Aqui você pode escolher um plano VIP que te dara beneficios como funções extras e mais personalizações do bot!" />
+
 </svelte:head>
 
 <div class="container flex flex-col justify-center items-center my-[50px]">
