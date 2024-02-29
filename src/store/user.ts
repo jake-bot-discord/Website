b1aaf5d909edd02ef69df0d2abe2c18a4811d622
Bianca de Castro <biancadcastro2501@gmail.com>
2024-02-28 22:47:50 -0300

    limpando código

--- src/store/user.ts
+++ src/store/user.ts
@@ -9,7 +9,6 @@ export const userStoredValue = readable(null, (set: any) => {
         try {
             const res = await axios.get(`${PUBLIC_API_URL}/auth/session`, { withCredentials: true })
     
-    
             set(res.data.updatedUserData)
         } catch(err) {
             console.log("Houve um erro ao pegar a session", err)
